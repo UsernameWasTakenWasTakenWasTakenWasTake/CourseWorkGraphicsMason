1688aa467ad7594cc0ef8599914d99293d432494
Mason Kelly-Scholes <23679754@stu.mmu.ac.uk>
2025-05-16 21:29:56 +0100

    I have addad rotation, translation and scaling

--- common/maths.cpp
+++ common/maths.cpp
@@ -1,1 +1,17 @@
+#include <common/maths.hpp>
+
+glm::mat4 Maths::translate(const glm::vec3& v)
+{
+    glm::mat4 translate(1.0f);
+    translate[3][0] = v.x, translate[3][1] = v.y, translate[3][2] = v.z;
+    return translate;
+}
+
+glm::mat4 Maths::scale(const glm::vec3& v)
+{
+    glm::mat4 scale(1.0f);
+    scale[0][0] = v.x; scale[1][1] = v.y; scale[2][2] = v.z;
+    return scale;
+}
+
 
